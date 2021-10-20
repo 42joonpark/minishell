29ae2c59f57e42796c508363297d0f762e3b7dc1
donpark <pdh6381@naver.com>
2021-10-20 20:19:04 +0900

    MOD: in while

--- src/loop.c
+++ src/loop.c
@@ -6,7 +6,7 @@
 /*   By: donpark <donpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:47:27 by joonpark          #+#    #+#             */
-/*   Updated: 2021/10/20 18:20:13 by donpark          ###   ########.fr       */
+/*   Updated: 2021/10/20 20:08:26 by donpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,12 @@ void	minishell_loop()
 	while (TRUE)
 	{
 		line = readline("> ");
+		if (pp_strcmp(line, "\0") == 0)
+			continue ;
 		args = pp_split(line, ' ');
-		//
+			//
 		pp_pwd(args);
+		pp_echo(args);
 		free(line);
 	}
 }
