ff09d8c0f6dcd93d151a56df7a9022ad0ef00b55
donpark <pdh6381@naver.com>
2021-10-20 20:17:32 +0900

    MOD: pp_echo(){ ... }

--- src/echo/echo.c
+++ src/echo/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonpark <joonpark@student.42.kr>          +#+  +:+       +#+        */
+/*   By: donpark <donpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 17:21:40 by joonpark          #+#    #+#             */
-/*   Updated: 2021/10/18 17:22:04 by joonpark         ###   ########.fr       */
+/*   Updated: 2021/10/20 19:50:08 by donpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 int	pp_echo(char **args)
 {
+	if (pp_strcmp(args[0], "echo"))
+		return (0);
+	if (pp_strcmp(args[1], "-n") == 0)
+		write(1, args[2], pp_strlen(args[2]));
+	else
+	{
+		write(1, args[1], pp_strlen(args[1]));
+		write(1, "\n", 1);
+	}
 	return (1);
 }
