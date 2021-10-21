e01030ec41f0055993e2e9141a5cb217226af115
donpark <pdh6381@naver.com>
2021-10-21 17:13:47 +0900

    ADD: export

--- src/loop.c
+++ src/loop.c
@@ -6,7 +6,7 @@
 /*   By: donpark <donpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:47:27 by joonpark          #+#    #+#             */
-/*   Updated: 2021/10/21 11:53:45 by donpark          ###   ########.fr       */
+/*   Updated: 2021/10/21 17:03:49 by donpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	minishell_loop(char **env)
 {
 	char	*line;
 	char	**args;
+	int		is_running;
 
-	while (TRUE)
+	is_running = TRUE;
+	while (is_running)
 	{
 		line = readline("> ");
 		if (pp_strcmp(line, "\0") == 0)
@@ -29,8 +31,12 @@ void	minishell_loop(char **env)
 			pp_echo(args);
 		if (pp_strcmp(args[0], "cd") == 0)
 			pp_cd(args);
+		if (pp_strcmp(args[0], "exit") == 0)
+			is_running = FALSE;
 		if (pp_strcmp(args[0], "env") == 0)
 			pp_env(env);
+		if (pp_strcmp(args[0], "export") == 0)
+			pp_export(args, env);
 		free(line);
 		//free(args);
 	}
