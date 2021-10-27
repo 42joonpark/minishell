1a820440dca0ecf0306011588be9aeafa28d0430
donpark <pdh6381@naver.com>
2021-10-27 10:53:13 +0900

    MOD: (src/export/export.c) check_value return value

--- src/export/export.c
+++ src/export/export.c
@@ -6,7 +6,7 @@
 /*   By: donpark <donpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 17:23:29 by joonpark          #+#    #+#             */
-/*   Updated: 2021/10/25 17:44:37 by donpark          ###   ########.fr       */
+/*   Updated: 2021/10/27 10:51:40 by donpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ static int	check_value(char *str, int i, int qq_cnt, int q_cnt)
 		i++;
 	}
 	if (i == (int)pp_strlen(str))
-		return (EXIT_FAILURE);
+		return (EXIT_SUCCESS);
 	while (str[i] != '\0')
 	{
 		if (str[i] == '"')
@@ -112,7 +112,6 @@ int	pp_export(char **args, t_list **exp_lst, t_list **env_lst)
 			return (EXIT_FAILURE);
 		}
 		val = get_value(args[1]);
-		printf("val: %s\n", val); fflush(stdout);
 		var_add_env(env_lst, args[1], key, val);
 		var_add_exp(exp_lst, args[1], key, val);
 	}
